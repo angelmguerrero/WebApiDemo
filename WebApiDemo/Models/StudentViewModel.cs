adcf5a37adfe4c1719f26b43d9007926568e7ee9
Angel Guerrero <34287872+angelmguerrero@users.noreply.github.com>
2020-09-06 21:09:16 -0500

    Web Api

---
+++ WebApiDemo/Models/StudentViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiDemo.Models
+{
+    public class StudentViewModel
+    {
+        public int Id { get; set; }
+        public string StudenName { get; set; }
+
+        public AddressViewModel Address { get; set; }
+        public StandardViewModel Standard { get; set; }
+    }
+}
